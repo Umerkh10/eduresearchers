c1629bf8122179c374f0fd74618c21cb27b5071a
umer <mumarkhan383@gmail.com>
2024-09-13 18:27:11 +0500

    smalldivider

--- src/app/(Home)/SmallDivider.tsx
+++ src/app/(Home)/SmallDivider.tsx
@@ -8,12 +8,12 @@ function SmallDivider() {
             <div data-aos='fade-up'  className='grid lg:grid-cols-3 grid-cols-1 p-4'>
                 <div className='col-span-2'>
                 <h1 className='font-semibold text-lg md:text-xl text-center md:text-start text-zinc-800'>Invest Your Money In Quality</h1>
-                <h2 className='pt-2 text-2xl md:text-4xl font-extrabold text-zinc-900 text-center md:text-start '>GET OUTSTANDING DISCOUNT ON YOUR FIRST ORDER</h2>
+                <h2 className='pt-2 text-xl md:text-4xl font-extrabold text-zinc-900 text-center md:text-start '>GET OUTSTANDING DISCOUNT ON YOUR FIRST ORDER</h2>
                 
                 <div className='grid md:grid-cols-2 grid-cols-1  mt-4'>
-                    <div className='mt-2 w-4/5 bg-white rounded-lg'><input className='py-4 outline-none w-full rounded-lg px-2' type="email" name='email' placeholder='Enter Your Email' /> </div>
+                    <div className='mt-2 w-4/5 bg-white rounded-lg md:ml-0 ml-10'><input className='py-4 outline-none w-full rounded-lg px-2' type="email" name='email' placeholder='Enter Your Email' /> </div>
 
-                    <div className='mt-2'><button className='bg-orange-500 py-4 px-6 rounded-lg text-white font-medium w-1/2 scale-90 hover:scale-95 lg:scale-95 lg:hover:scale-100 transition ease-in duration-200 delay-200 hover:shadow-xl'> Get a Discount</button> </div>
+                    <div className='mt-2 ml-0 md:ml-20'><button className='bg-orange-500 py-4 px-6 rounded-lg text-white font-medium w-3/4 md:w-1/2 scale-90 hover:scale-95 lg:scale-95 lg:hover:scale-100 transition ease-in duration-200 delay-200 hover:shadow-xl'> Get a Discount</button> </div>
                 </div>
 
                 </div>
