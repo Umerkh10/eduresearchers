1c15dca4dfbf5c1b05fe5e7f3fea8b02497d0905
umer <mumarkhan383@gmail.com>
2024-09-27 12:36:45 +0500

    withoutpartytown

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -6,8 +6,7 @@ import { ThemeProvider } from "./ThemeProvider";
 import { Toaster } from "sonner";
 import Tawkto from "@/lib/Tawkto";
 import Footer from "./(Home)/Footer";
-import Head from "next/head";
-import { Partytown } from '@builder.io/partytown/react';
+
 
 
 
@@ -42,11 +41,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-         <Head>
-       
-        <Partytown debug={true} forward={['dataLayer.push']} />
-        <script src="https://example.com/analytics.js" type="text/partytown" />
-      </Head>
+  
       <Tawkto/>
       <body
       
